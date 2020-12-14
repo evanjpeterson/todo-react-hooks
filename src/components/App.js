e068b2aad35f833e2e7c615adcaf18f4a66b8698
Evan <evan@evanjp.com>
2020-12-13 20:08:37 -0800

    toss in styled-components

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 import { TodoList } from "./TodoList";
 import { TodoProvider } from "./TodoProvider";
 
-import "./App.css";
-
 export const App = () => (
   <TodoProvider>
-    <div className="app">
-      <header className="app-header">To-do</header>
+    <StyledApp>
+      <header>To-do</header>
       <TodoList />
-    </div>
+    </StyledApp>
   </TodoProvider>
 );
+
+const StyledApp = styled.div`
+  text-align: center;
+
+  header {
+    background-color: #282c34;
+    min-height: 10vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  }
+`;
